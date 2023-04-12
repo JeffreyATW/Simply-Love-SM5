bc55e9f5eb1d25bbce286736685769b65c406156
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2023-04-12 12:43:34 -0700

    Fix current song single line, add backspace quick restart

--- BGAnimations/ScreenSelectMusic overlay/banner.lua
+++ BGAnimations/ScreenSelectMusic overlay/banner.lua
@@ -109,6 +109,14 @@ else
 	-- do nothing
 end
 f:destroy()
+f = RageFileUtil.CreateRageFile()
+if f:Open("Save/Out/SongInfoUpload.txt", 2) then
+	--local mods = GAMESTATE:GetPlayerState(0):GetPlayerOptionsString(2)
+	f:Write("")
+else
+	-- do nothing
+end
+f:destroy()
 --------------------------------------------------------------------------------------
 
 return t
